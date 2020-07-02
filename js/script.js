5bc3632aef7b4807a423c0479654948e5cb90506
Sudhanshu7sb <sudhanshu7.sb@gmail.com>
2020-07-02 22:40:33 +0530

    Solve reset message

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
-  displayEndTime(then);
+  displayEndTime(then,seconds);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -32,12 +32,12 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
 }
 
-function displayEndTime(timestamp) {
+function displayEndTime(timestamp,seconds) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At : ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = seconds ? `Be Back At : ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}` : "";
 }
 
 function startTimer() {
